83f8d1e72676fc2d99bc0bac5c5d929fa130e20e
EleaPicard <75881385+EleaPicard@users.noreply.github.com>
2020-12-23 21:42:37 +0100

    Add files via upload

--- board1.cpp
+++ board1.cpp
@@ -8,15 +8,18 @@ using namespace std;
 int main()
 {
   int size=200;
-  int level;
-  cout << "Please enter the level between 1 and 3" << endl;
-  cin >> level;
   int tab[size][size];
   int l, c;
   int r;
   int i,e;
 
-  
+  /*Choice of level between 1 and 3 
+  * i is the percentage of items
+  * 100-e is the percentage of enemies*/
+
+  int level;
+  cout << "Please enter the level between 1 and 3" << endl;
+  cin >> level;
   if (level==1)
     {
       i=40;
@@ -32,7 +35,11 @@ int main()
       i=30;
       e=60;
     }
-  
+  /*Creation of the board 
+  * 1 corresponds to an enemy
+  * 2 corresponds to an item
+  * -1 corresponds to an empty location*/
+ 
   for(l=0; l<size; l++)
     {
       for(c=0; c<size; c++)
